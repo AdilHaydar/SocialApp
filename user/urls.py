2fe18159d3ebfafb084c2792b40c0d43f915a9a6
Adil Haydar <47712899+AdilHaydar@users.noreply.github.com>
2021-02-10 16:17:30 +0300

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,15 +1,21 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
-#from .views import RegisterView
-app_name = "user"
+
 
 
 urlpatterns = [
     path('login/',views.loginUser,name = "login"),
     path('register/',views.register,name = "register"),
     path('logout/',views.logoutUser,name = "logout"),
-    path('panel/<str:username>',views.userPanel,name="userPanel"),
-    path('ajax/follow/',views.follow,name="follow"),
-    path('ajax/unfollow/',views.unfollow,name="unfollow"),
+    path('edit/<str:username>',views.change_user,name="user-edit"),
+    path('view/<str:username>',views.view_user,name="user-view"),
+    path('set-perm/',views.view_permission,name = "set-perm"),
+    path('add-delete-perm/<int:pk>',views.change_permission,name='add-delete-perm'),
+    path('add-group/',views.add_group,name='add-group'),
+    path('change-group/',views.change_group,name='change-group'),
+    path('remove-group/',views.remove_group,name='remove-group'),
+    path('show-group-members/<str:name>',views.show_group_members,name='show-group-members'),
+    path('edit-user/<str:username>',views.edit_user,name='edit-user'),
+    path('view-other-user/<str:username>',views.view_other_user,name='view-other-user')
+    
 ]
